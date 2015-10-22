cca5e87ac8e653a34c8f13c0619f64ad70eed080
weifan <p988744@gmail.com>
2015-10-22 22:16:46 +0800

    完成部落格基本功能

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,20 +4,46 @@ class BlogsController < ApplicationController
 	end
 
 	def show
+		@blog = Blog.find(params[:id])
 	end
 
 	def new
+		@blog = Blog.new
 	end
 	 
 	def edit
+		@blog = Blog.find(params[:id])
 	end
 
 	def create
+		@blog = Blog.create(blog_params)
+
+		if @blog.save
+		  redirect_to blogs_path
+		else
+		  render :new
+		end
 	end
 
 	def update
+		@blog = Blog.find(params[:id])
+
+		if @blog.update(blog_params)
+		  redirect_to blogs_path, notice: "修改文章成功"
+		else
+		  render :edit
+		end
 	end
 
 	def destroy
+		@blog = Blog.find(params[:id])
+		@blog.destroy
+		redirect_to blogs_path, alert: "文章已刪除"
+	end
+
+	private
+	
+	def blog_params 
+	  params.require(:blog).permit(:title, :categroies, :content)
 	end
 end
