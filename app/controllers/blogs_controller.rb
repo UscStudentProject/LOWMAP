e2c987fa70ce0c4a2a27ff05a8b3efaaba5b1d8b
weifan <p988744@gmail.com>
2015-10-22 21:22:11 +0800

    在blog_controller新增五大功能

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,23 @@
 class BlogsController < ApplicationController
 	def index
-		
+		@blogs = Blog.all
+	end
+
+	def show
+	end
+
+	def new
+	end
+	 
+	def edit
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
 	end
 end
